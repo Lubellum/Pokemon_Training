a69acd021b531382a7fa0baebd8ae082d5c802d3
Lubellum <90450493+Lubellum@users.noreply.github.com>
2023-03-11 17:42:59 +0900

    インデントの調整、不調な記述の削除

--- pokemon_training.cpp
+++ pokemon_training.cpp
@@ -14,13 +14,13 @@ int inputaction2();
 
 void Battle(CMonster * pokemon1, CMonster* pokemon2)
 {
-//	int pokemon1AttackPoint = pokemon1->Attack();
-//	int pokemon1result = pokemon2->Guard(pokemon1AttackPoint);
-//	pokemon2->Damage(pokemon1result);
-//
-//	int pokemon2AttackPoint = pokemon2->Attack();
-//	int pokemon2result = pokemon1->Guard(pokemon2AttackPoint);
-//	pokemon1->Damage(pokemon2result);
+	int pokemon1AttackPoint = pokemon1->Attack();
+	int pokemon1result = pokemon2->Guard(pokemon1AttackPoint);
+	pokemon2->Damage(pokemon1result);
+
+	int pokemon2AttackPoint = pokemon2->Attack();
+	int pokemon2result = pokemon1->Guard(pokemon2AttackPoint);
+	pokemon1->Damage(pokemon2result);
 }
 
 
